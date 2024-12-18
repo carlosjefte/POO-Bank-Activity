15dd8d8c4986341834197f23251aac9f999a95d7
Carlos Jefté <134069354+carlosjefte@users.noreply.github.com>
2024-12-18 15:33:26 -0300

    first commit

---
+++ class_activity/bank_services/conta_poupanca.py
@@ -0,0 +1,8 @@
+from bank_services.conta import Conta
+
+class ContaPoupanca(Conta): 
+  def __init__(self, numero):
+    super().__init__(numero)
+
+  def render_juros(self, taxa):
+    self.creditar(self.ver_saldo() * taxa)
---
+++ class_activity/student.py
@@ -0,0 +1,10 @@
+
+class Student:
+  __school_name = "XYZ School"
+
+  def __init__(self, name, age):
+    self.__name = name
+    self.__age = age
+
+  def __dysplay():
+    print("This method is private.")
